99fafe9897c3509da6410b99bf00e80f430a3b3e
mateuszlewandowicz <lewandowicz.mateusz@outlook.com>
2021-02-04 17:34:55 +0100

    new case navigation menu

---
+++ js.js
@@ -0,0 +1,21 @@
+const nav = document.querySelector('nav');
+const burger = document.querySelector('.burger');
+const burgerIco = document.querySelector('.fa-bars');
+const closeIco = document.querySelector('.fa-times');
+
+burger.addEventListener('click', function () {
+
+    nav.classList.toggle('active');
+    burger.classList.toggle('active');
+
+    if(burger.classList.contains('active')) {
+        
+        burgerIco.classList.add('hide');
+        closeIco.classList.remove('hide');
+        
+    } else {
+       burgerIco.classList.remove('hide');
+       closeIco.classList.add('hide');
+   
+    };
+});
